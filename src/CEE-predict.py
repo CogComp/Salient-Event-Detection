503527d09b51835cd53cb05b27d4bbec3bb8a19e
DishaJindal <dishajindal11@gmail.com>
2020-09-08 02:17:06 -0700

    predictions

--- src/CEE-predict.py
+++ src/CEE-predict.py
@@ -25,18 +25,11 @@ parser.add_argument('model_to_test', help='Model Absolute Path', type=str)
 parser.add_argument('out_file', help='Model Absolute Path', type=str)
 
 # Data Related
-parser.add_argument('-bs_train', '--train_batch_size', type=int, default=16, help='Training Batch Size across GPUs')
-parser.add_argument('-bs_val', '--val_batch_size', type=int, default=16, help='Validation Batch Size across GPUs')
 parser.add_argument('-bs_test', '--test_batch_size', type=int, default=16, help='Testing Batch Size across GPUs')
-parser.add_argument('-steps_train', '--train_steps', type=int, default=1476, help='Number of training steps')
-parser.add_argument('-steps_val', '--val_steps', type=int, default=380, help='Number of validation steps')
 parser.add_argument('-steps_test', '--test_steps', type=int, default=192, help='Number of testing steps')
-parser.add_argument('-w_train', '--num_workers_train', type=int, default=2, help='Number of workers to load training data')
-parser.add_argument('-w_val', '--num_workers_val', type=int, default=2, help='Number of workers to load validation data')
 parser.add_argument('-w_test', '--num_workers_test', type=int, default=2, help='Number of workers to load test data')
-parser.add_argument('--train_data_path', default='/shared/djjindal/CEE-data/train_set/', help='Train Data Path')
-parser.add_argument('--val_data_path', default='/shared/djjindal/CEE-data/val_set/', help='Validation Data Path')
-parser.add_argument('--test_data_path', default='/shared/djjindal/CEE-data/test_set/', help='Test Data Path')
+parser.add_argument('--test_dir', default='/shared/djjindal/CEE-data/test_set/', help='Test Data Dir Path')
+parser.add_argument('--test_file', default='full_data.json', help='Test File Name')
 
 # Model: Event Representation
 parser.add_argument('--freeze_bert', help='Freeze Bert?', action='store_true')
@@ -63,28 +56,15 @@ parser.add_argument('-sva_dim', '--p_key_query_dim', type=int, default=96, help=
 parser.add_argument('-dmn_passes', '--num_passes', type=int, default=2, help='Number of episodes of DMN')
 parser.add_argument('-dmn_hs', '--hs', type=int, default=256, help='Hidden Layer Size DMN')
 
-# Model Training
-parser.add_argument('-epochs', '--epochs', type=int, default=4, help='Number of Training Epochs')
-parser.add_argument("-param_init", default=0, type=float)
-parser.add_argument("-param_init_glorot", type=str2bool, nargs='?',const=True,default=True)
-parser.add_argument("-optim", default='adam', type=str)
-parser.add_argument("-lr", default=1e-4, type=float)
-parser.add_argument("-lr_bert", default=5e-4, type=float)
-parser.add_argument("-beta1", default= 0.9, type=float)
-parser.add_argument("-beta2", default=0.999, type=float)
-parser.add_argument("-warmup_steps", default=8000, type=int)
-parser.add_argument("-warmup_steps_bert", default=8000, type=int)
-parser.add_argument("-max_grad_norm", default=0, type=float)
-
 # Parse Arguments
 args = parser.parse_args()
 print("\nArguments...\n")
 print(args)
 numdocs = 51610
-def predict(file_name, out_filename, fname, model, device, bs=1):
+def predict(input_dir, input_fname, out_fname, model, device, bs=1):
     eval_obj=Eval()
-    outfile = open(out_filename, 'w')
-    iterable_predict_dataset = MyIterableDataset("", file_name, [fname], numdocs, "BERT")#46114 #2243#48360
+    outfile = open(out_fname, 'w')
+    iterable_predict_dataset = MyIterableDataset("", input_dir, [input_fname], numdocs, "BERT")#46114 #2243#48360
     loader =  DataLoader(iterable_predict_dataset, 
                         batch_size = bs,
                         collate_fn = my_collate,
@@ -127,8 +107,8 @@ def predict(file_name, out_filename, fname, model, device, bs=1):
 
 start = datetime.now()
 print("\nLoading Model...\n")
-model = LitNet.load_from_checkpoint('/shared/djjindal/IIE/trainedmodels/' + args.model_to_test, eval_obj=Eval(), args=args, mode=args.model_type)
+model = LitNet.load_from_checkpoint(args.model_to_test, eval_obj=Eval(), args=args, mode=args.model_type)
 dev = torch.device("cuda:"+args.gpus)
-predict('/shared/djjindal/ASD/data/full/test_parts_v3_srl/', '/shared/djjindal/ASD/data/full/preds/' + args.out_file, 'supfull_annotated', model.to(dev), dev)
+predict(args.test_dir, args.test_file , args.out_file, model.to(dev), dev)
 end = datetime.now()
 print((end-start).total_seconds())
